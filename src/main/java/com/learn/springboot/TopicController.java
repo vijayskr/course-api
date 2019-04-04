20bdda9810f95dcd1b4092cd06c3ae96619a32b0
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2019-04-03 22:09:59 -0500

    RestAPI's Added
    Added REST API Endpoints with the Controller

---
+++ src/main/java/com/learn/springboot/TopicController.java
@@ -0,0 +1,43 @@
+package com.learn.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicservice;
+	
+	@RequestMapping(method=RequestMethod.GET, value="/topics")
+	public List<Topic> getAllTopics() {
+		return topicservice.getAllTopics();
+	}
+	
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicservice.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicservice.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicservice.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicservice.deleteTopic(id);
+	}
+}
